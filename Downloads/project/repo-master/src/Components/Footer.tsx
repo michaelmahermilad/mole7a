cb68cccc92a8036751c5678621787ee9fe573bd6
unknown <mikaelmaaher2017@gmail.com>
2026-05-29 22:05:31 +0300

    all

--- Downloads/project/repo-master/src/Components/Footer.tsx
+++ Downloads/project/repo-master/src/Components/Footer.tsx
@@ -15,7 +15,7 @@ function Footer() {
 		<ul className="self-center py-6 space-y-4 text-center sm:flex sm:space-y-0 sm:justify-around sm:space-x-4 lg:flex-1 lg:justify-start">
 			<li><a  href='/services'>خدمات كنسية</a></li>
 			<li><a  href='/church-fathers'>اباء الكنيسة</a></li>
-			<li><a  href='/about'>تاريخ الكنيسة</a></li>
+			<li><a  href='/about'>تاريخ الكggggنيسة</a></li>
 			<li><a href='/church-mother-mary-google-map-location'>الموقع الجغرافي</a></li>
 		 
  		</ul>
